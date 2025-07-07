eeb6e6e5a470212a41041b1514f8bc468527eab1
MrRomero123 <luisromerop029@gmail.com>
2025-07-07 11:02:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,91 +2,76 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// CONEXIÓN A BASE DE DATOS
-$host = "34.71.26.50"; 
-$user = "root"; 
-$password = "123456"; 
-$database = "examen";
+$conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "examen");
 
-$conexion = mysqli_connect($host, $user, $password, $database);
-
+// Verificar conexión
 if (!$conexion) {
-  die("Error de conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 
-// CONSULTA SQL
 $consulta = "SELECT 
+    p.id_persona,
     p.nombre,
     p.edad,
     p.correo,
     p.fecha_nacimiento,
-    p.anio_nacimiento,
     o.descripcion AS origen,
+    p.anio_nacimiento,
     p.telefonos
-  FROM Persona p
-  JOIN Origen o ON p.id_origen = o.id_origen";
+FROM Persona p
+JOIN Origen o ON p.id_origen = o.id_origen";
 
 $resultado = mysqli_query($conexion, $consulta);
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
   <meta charset="utf-8">
   <title>Catálogo de Personas</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-    crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 </head>
 <body>
-  <div class="container mt-5">
-    <div class="jumbotron">
-      <h1 class="display-4">Catálogo de Personas</h1>
-      <p class="lead">Aplicación simple PHP + MySQL para mostrar personas con Bootstrap</p>
-      <hr class="my-4">
-    </div>
-
-    <table class="table table-striped table-bordered table-responsive">
-      <thead class="thead-dark">
-        <tr>
-          <th>Nombre</th>
-          <th>Edad</th>
-          <th>Correo</th>
-          <th>Fecha de Nacimiento</th>
-          <th>Año de Nacimiento</th>
-          <th>Origen</th>
-          <th>Teléfonos</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if ($resultado && mysqli_num_rows($resultado) > 0) {
-          while ($fila = mysqli_fetch_assoc($resultado)) {
-            echo "<tr>
-              <td>{$fila['nombre']}</td>
-              <td>{$fila['edad']}</td>
-              <td>{$fila['correo']}</td>
-              <td>{$fila['fecha_nacimiento']}</td>
-              <td>{$fila['anio_nacimiento']}</td>
-              <td>{$fila['origen']}</td>
-              <td>{$fila['telefonos']}</td>
-            </tr>";
-          }
-        } else {
-          echo "<tr><td colspan='7'>No hay datos disponibles.</td></tr>";
-        }
-        ?>
-      </tbody>
-    </table>
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Catálogo de Personas</h1>
+    <p class="lead">Ejemplo simple de PHP + MySQL para listar personas</p>
+    <hr class="my-4">
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-    crossorigin="anonymous"></script>
+  <table class="table table-striped table-bordered">
+    <thead class="thead-dark">
+      <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Edad</th>
+        <th>Correo</th>
+        <th>Fecha Nacimiento</th>
+        <th>Año Nacimiento</th>
+        <th>Origen</th>
+        <th>Teléfonos</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      if ($resultado && mysqli_num_rows($resultado) > 0) {
+          while ($fila = mysqli_fetch_assoc($resultado)) {
+              echo "<tr>
+                      <td>{$fila['id_persona']}</td>
+                      <td>{$fila['nombre']}</td>
+                      <td>{$fila['edad']}</td>
+                      <td>{$fila['correo']}</td>
+                      <td>{$fila['fecha_nacimiento']}</td>
+                      <td>{$fila['anio_nacimiento']}</td>
+                      <td>{$fila['origen']}</td>
+                      <td>{$fila['telefonos']}</td>
+                    </tr>";
+          }
+      } else {
+          echo "<tr><td colspan='8'>No se encontraron resultados.</td></tr>";
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
 </body>
 </html>
-
