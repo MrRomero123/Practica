14baefdc192889078c335a90c449b29940719d40
MrRomero123 <luisromerop029@gmail.com>
2025-07-07 11:07:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,76 +2,91 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "examen");
+// CONEXIÓN A BASE DE DATOS
+$host = "34.71.26.50"; 
+$user = "root"; 
+$password = "123456"; 
+$database = "examen";
 
-// Verificar conexión
+$conexion = mysqli_connect($host, $user, $password, $database);
+
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+  die("Error de conexión: " . mysqli_connect_error());
 }
 
+// CONSULTA SQL
 $consulta = "SELECT 
-    p.id_persona,
     p.nombre,
     p.edad,
     p.correo,
     p.fecha_nacimiento,
-    o.descripcion AS origen,
     p.anio_nacimiento,
+    o.descripcion AS origen,
     p.telefonos
-FROM Persona p
-JOIN Origen o ON p.id_origen = o.id_origen";
+  FROM Persona p
+  JOIN Origen o ON p.id_origen = o.id_origen";
 
 $resultado = mysqli_query($conexion, $consulta);
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
   <meta charset="utf-8">
   <title>Catálogo de Personas</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+    crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-  <div class="jumbotron mt-4">
-    <h1 class="display-4">Catálogo de Personas</h1>
-    <p class="lead">Ejemplo simple de PHP + MySQL para listar personas</p>
-    <hr class="my-4">
+  <div class="container mt-5">
+    <div class="jumbotron">
+      <h1 class="display-4">Catálogo de Personas</h1>
+      <p class="lead">Aplicación simple PHP + MySQL para mostrar personas con Bootstrap</p>
+      <hr class="my-4">
+    </div>
+
+    <?php
+    if ($resultado && mysqli_num_rows($resultado) > 0) {
+      while ($fila = mysqli_fetch_assoc($resultado)) {
+        echo "
+        <h3 class='mt-5'>Datos personales de {$fila['nombre']}</h3>
+        <table class='table table-bordered table-striped'>
+          <thead class='thead-light'>
+            <tr>
+              <th>Edad</th>
+              <th>Correo</th>
+              <th>Fecha de Nacimiento</th>
+              <th>Año de Nacimiento</th>
+              <th>Origen</th>
+              <th>Teléfonos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{$fila['edad']}</td>
+              <td>{$fila['correo']}</td>
+              <td>{$fila['fecha_nacimiento']}</td>
+              <td>{$fila['anio_nacimiento']}</td>
+              <td>{$fila['origen']}</td>
+              <td>{$fila['telefonos']}</td>
+            </tr>
+          </tbody>
+        </table>";
+      }
+    } else {
+      echo "<p>No hay datos disponibles.</p>";
+    }
+    ?>
+
   </div>
 
-  <table class="table table-striped table-bordered">
-    <thead class="thead-dark">
-      <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Edad</th>
-        <th>Correo</th>
-        <th>Fecha Nacimiento</th>
-        <th>Año Nacimiento</th>
-        <th>Origen</th>
-        <th>Teléfonos</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      if ($resultado && mysqli_num_rows($resultado) > 0) {
-          while ($fila = mysqli_fetch_assoc($resultado)) {
-              echo "<tr>
-                      <td>{$fila['id_persona']}</td>
-                      <td>{$fila['nombre']}</td>
-                      <td>{$fila['edad']}</td>
-                      <td>{$fila['correo']}</td>
-                      <td>{$fila['fecha_nacimiento']}</td>
-                      <td>{$fila['anio_nacimiento']}</td>
-                      <td>{$fila['origen']}</td>
-                      <td>{$fila['telefonos']}</td>
-                    </tr>";
-          }
-      } else {
-          echo "<tr><td colspan='8'>No se encontraron resultados.</td></tr>";
-      }
-      ?>
-    </tbody>
-  </table>
-</div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+    crossorigin="anonymous"></script>
 </body>
 </html>
